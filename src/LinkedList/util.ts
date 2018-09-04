75993020db69718ea301d03b06445a3238b35d97
BengBu-YueZhang <zhangyue_coder@163.com>
2018-09-05 07:51:17 +0800

    add

--- src/LinkedList/util.ts
+++ src/LinkedList/util.ts
@@ -167,3 +167,183 @@ function removeElements (head: LinkedListNode, val: number): LinkedListNode {
   }
   return head
 }
+
+/**
+ * 【一个重要的思想，生成一个假头】使用 new ListNode(0), 生成一个虚假的头部
+ * 最后返回的结果的时候可以忽略这个家头
+ * 【另一个重要的思想，同时保留链表头部和尾部的引用】
+ * let newHead = new ListNode(0)
+ * let newTail = newHead
+ * let newNode = new ListNode(1)
+ * newTail.next = newNode
+ * newTail = newTail.next
+ */
+
+/**
+ * 奇偶链表
+ * 奇偶两条链，就是使用 new ListNode(0),这种方式生成的
+ */
+function oddEvenList (head: LinkedListNode): LinkedListNode {
+  let oddHead: LinkedListNode = new LinkedListNode(0)
+  let evenHead: LinkedListNode = new LinkedListNode(0)
+  let oddTail: LinkedListNode = oddHead
+  let evenTail: LinkedListNode = evenHead
+  let index: number = 1
+  
+  while (head) {
+      // 链接不同奇偶两条链
+      // 这里默认开头是1，所以index从1开始
+      if (index % 2 !== 0) {
+          oddTail.next = head
+          oddTail = oddTail.next
+      } else {
+          evenTail.next = head
+          evenTail = evenTail.next
+      }
+      head = head.next
+      index += 1
+  }
+  
+  // 偶数链的结尾是null，因为是尾部
+  evenTail.next = null
+  // 忽略到假头
+  oddTail.next = evenHead.next
+  
+  return oddHead.next
+}
+
+/**
+ * 回文链表
+ * 一开始我的相反是反转整个链表进行对比，但是发现如果反转整个链表，就相当于改变整个链表，没有办法进行对比了
+ * 所以取中点，中点以后的进行反转对比，但是需要注意这几种情况
+ * [] 链表为空，是回文链表
+ * [0, 0] 依然是回文链表
+ * 获取中点的方式是使用快慢指针
+ */
+function isPalindrome (head: LinkedListNode): boolean {
+  if (!head) {
+    return true
+  }
+
+  // 通过快慢指针获取中点
+  let fast: LinkedListNode = head
+  let slow: LinkedListNode = head
+
+  // 链表中点
+  let middle = null
+
+  // 循环结束后慢节点就是链表的中点
+  while (fast && fast.next && fast.next.next) {
+      fast = fast.next.next
+      slow = slow.next
+  }
+
+  // 一个和两个的情况
+  if (fast === slow) {
+      if (!fast.next) {
+          return true
+      } else if ( fast.val === fast.next.val ) {
+          return true
+      } else {
+          return false
+      }
+  }
+
+  // middle保持对中点的引用
+  // slow往后移动
+  middle = slow
+
+  // 反转中点以后的链表
+  function reverse () {
+      let a = slow.next
+      let b = slow.next.next
+      a.next = middle
+      slow.next = b
+      middle = a
+  }
+
+  while (slow && slow.next) {
+      reverse()
+  }
+
+  // 从头部和中点开始对比
+  while (head && middle) {
+      
+      if (head.val !== middle.val) {
+          return false
+      }
+      
+      head = head.next
+      middle = middle.next
+      
+  }
+
+  return true    
+}
+
+/**
+ * 合并两个链表
+ */
+function mergeTwoLists (l1: LinkedListNode, l2: LinkedListNode): LinkedListNode {
+  // 头部的引用
+  let newHead: LinkedListNode = new LinkedListNode(0)
+  // 尾部的引用
+  let newTail: LinkedListNode = newHead
+  
+  while (l1 && l2) {
+      if (l1.val < l2.val) {
+          newTail.next = l1
+          l1 = l1.next
+      } else {
+          newTail.next = l2
+          l2 = l2.next
+      }
+      // 始终指向尾部
+      newTail = newTail.next
+  }
+  
+  if (!l1) {
+      newTail.next = l2
+  }
+  
+  if (!l2) {
+      newTail.next = l1
+  }
+  
+  return newHead.next
+}
+
+/**
+ * 两个链表进行相加
+ */
+function addTwoNumbers (l1: LinkedListNode, l2: LinkedListNode): LinkedListNode {
+    let newHead: LinkedListNode = new LinkedListNode(0)
+    let newTail: LinkedListNode = newHead
+    // carry是近卫，8 + 8 = 16 ，进位为1
+    let carry: number = 0
+    while (l1 || l2) {
+        let a: number = l1 ? l1.val : 0
+        let b: number = l2 ? l2.val : 0
+        // val是保留的位
+        let val: number = (a + b + carry) % 10
+        carry = Math.floor((a + b + carry) / 10)
+        let newNode = new LinkedListNode(val)
+        newTail.next = newNode
+        newTail = newTail.next
+        if (l1) {
+            l1 = l1.next
+        }
+        if (l2) {
+            l2 = l2.next
+        }
+    }
+    if (carry !== 0) {
+        // 注意最后一位的进位
+        let newNode: LinkedListNode = new LinkedListNode(carry)
+        newTail.next = newNode
+        newTail = newTail.next
+    }
+    
+    return newHead.next
+}
+
