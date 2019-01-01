30c82d0931205e2a63e7a5f3726c3a7bb64125b2
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-01 22:25:23 +0800

    update

--- src/动态规划/Length_of_Longest_Fibonacci_Subsequence.js
+++ src/动态规划/Length_of_Longest_Fibonacci_Subsequence.js
@@ -4,58 +4,58 @@
  * min = Math.min(fibonacci(start, n), fibonacci(start, getNext(n)))
  */
 
-var lenLongestFibSubseq = function (A) {
+var lenLongestFibSubseq = function(A) {
   let maxLen = 0
-  let result = []
-  const len = A.length
-
-  /**
-   * 获取下一个fibonacci数列的值
-   * @param {*} preValue 前一个fibonacci数列的值
-   * @param {*} startPos 开始查找的位置
-   */
-  const getNext = (preValue, startPos) => {
-    if (startPos >= len - 1) {
-      return undefined
+let result = []
+const len = A.length
+ 
+const getNext = (current, startPos) => {
+  if (startPos >= len - 1) {
+    return undefined
+  } else {
+    let sum = 0
+    if (current.length > 1) {
+      sum = current[current.length - 1] + current[current.length - 2]
     } else {
-      let sum = preValue + A[startPos]
-      let nextIndex = A.indexOf(sum)
-      if (nextIndex < startPos + 1) {
-        return getNext(preValue, startPos + 1)
-      } else {
-        return {
-          nextValue: A[startPos],
-          nextIndex: startPos,
-          afterNextValue: A[nextIndex],
-          afterNextIndex: nextIndex
-        }
+      sum = current[current.length - 1] + A[startPos]
+    }
+    let nextIndex = A.indexOf(sum)
+    if (nextIndex < startPos + 1) {
+      return getNext(current, startPos + 1)
+    } else {
+      return {
+        nextValue: A[startPos],
+        nextIndex: startPos,
+        afterValue: A[nextIndex],
+        afterIndex: nextIndex
       }
     }
   }
+}
 
-  const getFibonacci = (init, startPos) => {
-    let fibonacci = [...init]
-    let next = getNext(init[init.length - 1], startPos)
-    if (!next) {
-      result.push(fibonacci)
-    } else {
-      const { nextValue, afterNextValue, afterNextIndex } = next
-      // 使用next
-      getFibonacci([...fibonacci, nextValue, afterNextValue], afterNextIndex + 1)
-      // 不使用next
-      getFibonacci([...fibonacci], startPos + 1)
-    }
+const getFibonacci = (init, startPos) => {
+  let fibonacci = [...init]
+  let next = getNext(init, startPos)
+  if (!next) {
+    result.push(fibonacci)
+  } else {
+    const { nextValue, afterValue, afterIndex } = next
+    // 使用next
+    getFibonacci([...fibonacci, nextValue, afterValue], afterIndex + 1)
+    // 不使用next
+    getFibonacci([...fibonacci], startPos + 1)
   }
-  for (let i = 0; i < A.length - 2; i++) {
-    getFibonacci([A[i]], ++i)
-  }
+}
+for (let i = 0; i < A.length - 2; i++) {
+  getFibonacci([A[i]], i + 1)
+}
   let lens = result.map(item => item.length)
   console.log(result)
   return Math.max.apply(null, lens)
 };
 
 console.log(
-  lenLongestFibSubseq(
-    [2,4,5,6,7,8,11,13,14,15,21,22,34]
+  lenLongestFibSubseq( 
+    [2,4,7,8,9,10,14,15,18,23,32,50]
   )
 )
