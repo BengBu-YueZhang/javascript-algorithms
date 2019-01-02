9fea3a7825aa3b18fc92c7564de05d2dd8dd63b7
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-02 16:11:26 +0800

    反转字符串中的元音字母

---
+++ src/数组/反转字符串中的元音字母.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+  // a[ei]、e[i:]、i[ ai]、o[eu]、u[ju:]
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  const len = s.length
+  let arr = s.split('')
+  let start = 0
+  let end = len - 1
+  while (start < end) {
+      if (vowels.indexOf(arr[start]) > -1 && vowels.indexOf(arr[end]) > -1) {
+          let temp = arr[start]
+          arr[start] = arr[end]
+          arr[end] = temp
+          start += 1
+          end -= 1
+      } else if (vowels.indexOf(arr[start]) === -1 && vowels.indexOf(arr[end]) > -1) {
+          start += 1
+      } else if (vowels.indexOf(arr[start]) > -1 && vowels.indexOf(arr[end]) === -1) {
+          end -= 1
+      } else {
+          start += 1
+          end -= 1
+      }
+  }
+  return arr.join('')
+};
