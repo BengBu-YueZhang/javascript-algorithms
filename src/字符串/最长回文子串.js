894a046eaa22904af9c0b8114d3e9a3c381242a7
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-06 18:40:35 +0800

    最长回文子串

--- src/字符串/最长回文子串.js
+++ src/字符串/最长回文子串.js
@@ -4,6 +4,10 @@
  */
 var longestPalindrome = function(s) {
   // Manacher算法
+  // Manacher算法的介绍, https://www.cnblogs.com/grandyang/p/4475985.html
+  // p[n]等于 n位置的回文字符串的半径
+  // 🌟 最长子串的长度是p[n] - 1
+  // n - p[n]
   // 动态规划
   // 中心扩展算法
 };
