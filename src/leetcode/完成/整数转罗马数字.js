685a2dd7ecaf7575fb9f1badf2ea0dfefe2c5295
BengBu-YueZhang <zhangyue_coder@163.com>
2019-01-14 00:10:38 +0800

    整数转罗马数字

---
+++ src/leetcode/完成/整数转罗马数字.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+  let result = ''
+  let dictionary = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000
+  }
+  // if (num % dictionary.M > 0) {
+  //     result += 'M'
+  //     num = num % dictionary.M
+  // }
+  // if (num % dictionary.D > 0) {
+  //     result += 'M'
+  //     num = num % dictionary.M
+  // }
+};
