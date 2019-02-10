91a84bba405865a762cf305efece1be85d9e766e
BengBu-YueZhang <zhangyue_coder@163.com>
2019-02-10 17:54:09 +0800

    二叉树的广度优先搜索

---
+++ src/二叉树/二叉树的层次遍历.js
@@ -0,0 +1,28 @@
+// 广度优先搜索
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  let result = []
+  const traversing = (node, level) => {
+    if (!node) return
+    if (!result[level]) result[level] = []
+    result[level].push(node.val)
+    if (node.left) {
+      traversing(node.left, level + 1)
+    }
+    if (node.right) {
+      traversing(node.right, level + 1)
+    }
+  }
+  traversing(root, 0)
+  return result
+};
