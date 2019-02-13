69a6cf06b63508c6776ce8530537a5960504e92f
BengBu-YueZhang <zhangyue_coder@163.com>
2019-02-13 17:51:52 +0800

    每个节点的右向指针 II

---
+++ src/leetcode/完成/每个节点的右向指针 II.js
@@ -0,0 +1,41 @@
+/**
+ * 这道题，应该是有问题的，题库中已经搜索不到这道题目了，解法同前一题类似，广度优先搜索即可
+ */
+
+/**
+ * Definition for binary tree with next pointer.
+ * function TreeLinkNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = this.next = null;
+ * }
+ */
+
+/**
+ * @param {TreeLinkNode} root
+ * @return {void} Do not return anything, modify tree in-place instead.
+ */
+var connect = function(root) {
+  // BFS
+  let result = []
+const traversing = (node, level) => {
+  if (!node) return
+  if (!result[level]) result[level] = []
+  result[level].push(node)
+  if (node.left) {
+    traversing(node.left, level + 1)
+  }
+  if (node.right) {
+    traversing(node.right, level + 1)
+  }
+}
+traversing(root, 0)
+  for (let i = 0 ; i < result.length; i++) {
+      for (let j = 0; j < result[i].length; j++) {
+          if (j + 1 < result[i].length) {
+              result[i][j].next = result[i][j + 1]
+          } else {
+              result[i][j].next = null
+          }
+      }
+  }
+};
