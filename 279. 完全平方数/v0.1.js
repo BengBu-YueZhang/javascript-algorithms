7b61149da5d24f52b896621c162a759070ab0f8e
BengBu-YueZhang <zhangyue_coder@163.com>
2020-02-23 14:34:55 +0800

    update: 完全平方数

--- 279. 完全平方数/v0.1.js
+++ 279. 完全平方数/v0.1.js
@@ -11,23 +11,40 @@ var numSquares = function(n) {
         squareNumber.push(i * i)
     }
 
-    const bfs = (num, queue) => {
+    const getKey = (keys) => {
+        return keys.sort((a, b) => a - b).join(',')
+    }
+
+    const bfs = (hash, num, queue) => {
         let tempQueue = []
 
         num += 1
 
         for (let i = 0; i < queue.length; i++) {
-            const { val, parent } = queue[i]
+            const { val, parent, key } = queue[i]
             let tempSum = parent + val
             if (tempSum > n) {
                 continue
             } else if (tempSum < n) {
+                let nextQueue = []
+                for (let i = 0; i < squareNumber.length; i++) {
+                    let newKey = getKey([...key, squareNumber[i]])
+                    if (
+                        tempSum + squareNumber[i] <= n &&
+                        !hash.has(newKey)
+                    ) {
+                        hash.set(newKey, true)
+                        nextQueue.push({
+                           val: squareNumber[i],
+                           parent: tempSum,
+                           key: newKey
+                        })
+                    }
+
+                }
                 tempQueue = [
                     ...tempQueue,
-                    ...squareNumber.map(n => ({
-                        val: n,
-                        parent: tempSum
-                    }))
+                    ...nextQueue
                 ]
             } else {
                 return num
@@ -35,13 +52,16 @@ var numSquares = function(n) {
         }
 
         if (tempQueue.length) {
-            return bfs(num, tempQueue)
+            return bfs(hash, num, tempQueue)
         }
-
     }
 
-    return bfs(num, squareNumber.map(n => ({
+    // 避免 1-9，9-1这样重复的计算
+    const hash = new Map()
+
+    return bfs(hash, num, squareNumber.map(n => ({
         val: n,
-        parent: 0
+        parent: 0,
+        key: [0, n]
     })))
 };
