35244ba3833a1b3d4b81624cf4717c2c4d5a93f7
BengBu-YueZhang <zhangyue_coder@163.com>
2020-02-27 22:18:28 +0800

    update: 更新字符串解码

--- 394.字符串解码/v0.1.js
+++ 394.字符串解码/v0.1.js
@@ -34,10 +34,13 @@ var decodeString = function(s) {
     const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     const tokens = s.split('')
 
+    let tempNumber = ''
+
     for (let i = 0; i < tokens.length; i++) {
         const token = tokens[i]
         if (numbers.includes(token)) {
-            numberStack.push(Number(token))
+            tempNumber += token
+            
         } else {
             if (token === ']') {
                 let temp = lettersStack.pop()
@@ -56,8 +59,12 @@ var decodeString = function(s) {
                     num--
                 }
                 lettersStack.push(temp2)
+            } else if (token === '[') {
+                numberStack.push(Number(tempNumber))
+                lettersStack.push(token) 
+                tempNumber = ''
             } else {
-               lettersStack.push(token) 
+                lettersStack.push(token) 
             }
         }
     }
