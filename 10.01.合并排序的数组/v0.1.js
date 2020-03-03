19d44aae576094b033ca00bf8c9705c94ce37b37
BengBu-YueZhang <zhangyue_coder@163.com>
2020-03-03 18:36:16 +0800

    update: 更新合并两个有序数组

--- 10.01.合并排序的数组/v0.1.js
+++ 10.01.合并排序的数组/v0.1.js
@@ -14,32 +14,31 @@
 var merge = function(A, m, B, n) {
     // 首先删除A数组后面碍事的0
     A.splice(m, A.length)
-    
+
     // 一些特殊情况的处理
     // 如果B的开头就大于A的末尾，直接把B全部添加到A的末尾即可
     // 如果B的长度为0，也是如此
-    if (B[0] > A[A.length - 1] || B.length === 0) {
+    if (B[0] > A[A.length - 1] || B.length === 0 || A.length === 0) {
         for (let i = 0; i < B.length; i++) {
             A.push(B[i])
         }
     } else {
         let index = 0
         let head = B.shift()
-        while (B.length > 0 && index < m) {
+        while (B.length >= 0 && index < A.length) {
             if (head < A[index]) {
                 A.splice(index, 0, head)
                 head = B.shift()
             }
             index += 1
         }
+        if (typeof head === 'number') {
+            A.push(head)
+        }
         if (B.length) {
-            if (head) {
-                A.push(head)
-            }
             for (let i = 0; i < B.length; i++) {
                 A.push(B[i])
             }
         }
     }
-
 };
